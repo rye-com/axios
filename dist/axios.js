968b5b87f10d257dbcd07047da150f73ca8d7dc6
Dima Z <dmitry.zabello@gmail.com>
2023-01-29 13:40:49 -0800

    :x
    :x
    
    This reverts commit 9a887a5d57041e0a180690d0640882e3157fe14c.

--- dist/axios.js
+++ dist/axios.js
@@ -1,4 +1,4 @@
-// Axios v1.2.3-rye.4 Copyright (c) 2023 Matt Zabriskie and contributors
+// Axios v1.2.3-rye.3 Copyright (c) 2023 Matt Zabriskie and contributors
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
